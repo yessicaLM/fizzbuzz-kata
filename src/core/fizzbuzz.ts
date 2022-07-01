25c3745f75810f0b4958ea35b1b455af58582edc
yessica <yessica@leanmind.es>
2022-07-01 10:59:13 +0100

    Code refactorings

--- src/core/fizzbuzz.ts
+++ src/core/fizzbuzz.ts
@@ -1,6 +1,9 @@
+const isDivisibleBy3 = (number: number): boolean => number % 3 === 0;
+const isDivisibleBy5 = (number: number): boolean => number % 5 === 0;
+
 export const fizzbuzzCalculator = (number: number): string | number => {
-  if (number % 3 === 0 && number % 5 === 0) return 'fizzbuzz';
-  if (number % 3 === 0) return 'fizz';
-  if (number % 5 === 0) return 'buzz';
+  if (isDivisibleBy3(number) && isDivisibleBy5(number)) return 'fizzbuzz';
+  if (isDivisibleBy3(number)) return 'fizz';
+  if (isDivisibleBy5(number)) return 'buzz';
   return number;
 }
--- src/tests/fizzbuzz.test.ts
+++ src/tests/fizzbuzz.test.ts
@@ -1,31 +1,6 @@
 import { fizzbuzzCalculator } from '../core/fizzbuzz';
-// 1 -> 1 --> DONE
-// 3 -> fizz --> DONE
-// 5 -> buzz --> DONE
-// 15 -> fizzbuzz --> DONE
-// n % 3 = 0 -> fizz --> DONE
-// n % 5 = 0 -> buzz --> DONE
-// n % 15 = 9 -> fizzbuz --> DONE
-// other nums -> nums --> DONE
-
 
 describe('fizzbuzz kata', () => {
-  it('given 1 returns 1', () => {
-    expect(fizzbuzzCalculator(1)).toBe(1);
-  });
-
-  it('given 3 returns fizz', () => {
-    expect(fizzbuzzCalculator(3)).toBe('fizz');
-  });
-
-  it('given 5 returns buzz', () => {
-    expect(fizzbuzzCalculator(5)).toBe('buzz');
-  });
-
-  it('given 15 returns fizzbuzz', () => {
-    expect(fizzbuzzCalculator(15)).toBe('fizzbuzz');
-  });
-
   it('given a number divisible by 3 returns fizz', () => {
     expect(fizzbuzzCalculator(6)).toBe('fizz');
   });
