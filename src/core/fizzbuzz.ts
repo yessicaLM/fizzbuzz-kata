ca33204662fede81fd4a4181632c71040b999468
yessica <yessica@leanmind.es>
2022-07-01 10:45:54 +0100

    GREEN: given 15 returns fizzbuzz

--- src/core/fizzbuzz.ts
+++ src/core/fizzbuzz.ts
@@ -1,5 +1,6 @@
 export const fizzbuzzCalculator = (number: number): string | number => {
   if (number === 3) return 'fizz';
   if (number === 5) return 'buzz';
+  if (number === 15) return 'fizzbuzz';
   return 1;
 }
