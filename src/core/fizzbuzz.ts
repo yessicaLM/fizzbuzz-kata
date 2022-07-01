2b2fe7ca0b04961d7536746747860a1d47d45d33
yessica <yessica@leanmind.es>
2022-07-01 10:48:47 +0100

    GREEN: given a number divisible by 3 returns fizz

--- src/core/fizzbuzz.ts
+++ src/core/fizzbuzz.ts
@@ -1,6 +1,6 @@
 export const fizzbuzzCalculator = (number: number): string | number => {
-  if (number === 3) return 'fizz';
-  if (number === 5) return 'buzz';
   if (number === 15) return 'fizzbuzz';
+  if (number % 3 === 0) return 'fizz';
+  if (number === 5) return 'buzz';
   return 1;
 }
